4cfd9c3803668e6216b389848fe27f051b2b9efa
Tim Kovacic <kovacictim3@gmail.com>
2018-01-21 23:10:15 -0500

    Added Title Screen Basic Setup with Application.Quit() Functionality

---
+++ Prototype 18/Assets/Resources/Scripts/sceneManagement/titleManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class titleManager : MonoBehaviour {
+
+	public void closeApp()
+    {
+        Application.Quit();
+    }
+}
